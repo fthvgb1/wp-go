963a41f27ed74ea787798c723e0a35561866f804
xing <fthvgb1>
2022-09-23 21:58:24 +0800

    favicon缓存

--- middleware/cache.go
+++ middleware/cache.go
@@ -8,7 +8,7 @@ import (
 
 func SetStaticFileCache(c *gin.Context) {
 	f := strings.Split(strings.TrimLeft(c.FullPath(), "/"), "/")
-	if len(f) > 1 && helper.IsContainInArr(f[0], []string{"wp-includes", "wp-content"}) {
+	if len(f) > 0 && helper.IsContainInArr(f[0], []string{"wp-includes", "wp-content", "favicon.ico"}) {
 		c.Header("Cache-Control", "private, max-age=86400")
 	}
 	c.Next()
