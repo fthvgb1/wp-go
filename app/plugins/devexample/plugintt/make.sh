87f639da586e5e3224da1d5f4520247af55914b1
xing <fthvgb1@163.com>
2023-05-06 23:00:43 +0800

    示例

--- app/plugins/devexample/plugintt/make.sh
+++ app/plugins/devexample/plugintt/make.sh
@@ -2,5 +2,6 @@
 
 # copy plugintt to other dir and remove .dev suffix
 # note the go version and build tool flag must same to server build
-# eg: -gcflags all="-N -l" may used in ide debug
+# eg: -gcflags all="-N -l" --race may used in ide debug
+go mod tidy
 go build -buildmode=plugin -o xx.so main.go
